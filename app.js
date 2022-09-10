e8a9edc5d75bc8cb5f7bc95725155fbc11df5613
CrisGraciano <cristobalgraciano98@gmail.com>
2022-09-09 19:41:19 -0500

    inquirer working

--- app.js
+++ app.js
@@ -1,28 +1,22 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template');
+const inquirer = require('inquirer');
 
-const profileDataArgs = process.argv.slice(2);
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?',
+        }
+    ])
+    .then(answers => console.log(answers));
+    
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
 
-const [name, github] = profileDataArgs
+// const pageHTML = generatePage(name, github);
 
-fs.writeFile('./index.html', generatePage(name, github), err => {
-    if (err) throw new err;
+// fs.writeFile('./index.html', generatePage(name, github), err => {
+//     if (err) throw err;
 
-    console.log('Portfolio complete! Checkout index.html to see the output!');
-});
-
-
-// const printProfileData = profileDataArr => {
-//     // This
-//     for (let i = 0; i <profileDataArr.length; i++) {
-//     console.log(profileDataArr[i]);
-//     }
-
-//     console.log('================');
-
-//     // IS the same as this
-//     profileDataArr.forEach(profileItem => console.log(profileItem));
-// };
-
-// printProfileData(profileDataArgs);
-
+//     console.log('Portfolio complete! Checkout index.html to see the output!');
+// });
