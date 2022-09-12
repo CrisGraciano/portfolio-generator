14de2d76f23147c803c28933a7e041cdc74229d5
CrisGraciano <cristobalgraciano98@gmail.com>
2022-09-12 17:58:40 -0500

    fixed terminal issue and made all the page

--- src/page-template.js
+++ src/page-template.js
@@ -85,10 +85,13 @@ module.exports = templateData => {
     <html lang="en">
   
     <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <meta http-equiv="X-UA-Compatible" content="ie=edge">
-      <title>Portfolio Demo</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Portfolio Demo</title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
+        <link href="https://fonts.googleapis.com/css?family=Public+Sans:300i,300,500&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="style.css">
     </head>
   
     <body>
@@ -104,8 +107,9 @@ module.exports = templateData => {
          </header>
         <main class="container my-5">
              ${generateAbout(about)}
+             ${generateProjects(projects)}
         </main>
-        ${generateProjects(projects)}
+
         <footer class="container text-center py-3">
             <h3 class="text-dark">&copy; ${new Date().getFullYear()} by ${header.name}</h3>
         </footer>
